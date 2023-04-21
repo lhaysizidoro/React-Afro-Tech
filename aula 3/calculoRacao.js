e51fdcf919fb82d8c43159bc0241047ec1133f65
Lhays Izidoro <izidorolhays@gmail.com>
2023-04-21 13:07:07 -0300

    first commit

---
+++ aula 3/calculoRacao.js
@@ -0,0 +1,19 @@
+let button = document.getElementById("calcular");
+button.addEventListener("mouseout", mudarCor);
+button.addEventListener("click", calcular);
+let elementoResultado = document.getElementById("resultado");
+
+function calcular(){
+//interpolacao
+    elementoResultado.innerHTML = "O babbage deve comer 100g por dia"
+    let nomeCachorro = document.getElementById("nome").value;
+    //console.log(nome.nomeCachorro)
+
+    elementoResultado.innerHTML = `${nomeCachorro} deve comer 100g por dia`
+
+    //  elementoResultado.innerHTML = "<span id='racao'>oi</span>";
+//   <span id="resultado">"innerHTML"</span>
+}
+function mudarCor(){
+    button.style.backgroundColor= "blueviolet";
+}
